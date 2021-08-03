f4e78f5ada40f94e73beab3aefa237d7820ac303
metteec2 <chmettee@gmail.com>
2021-08-03 08:17:29 -0400

    Added constructors, fields, and getter and setter methods to classes in the users package

--- src/main/java/com/revature/p0/users/FacultyMember.java
+++ src/main/java/com/revature/p0/users/FacultyMember.java
@@ -1,4 +1,12 @@
 package com.revature.p0.users;
 
 public class FacultyMember extends User {
+
+    public FacultyMember(String userType, String firstName, String lastName, String username, String password, String email) {
+        super("Faculty", firstName, lastName, username, password, email);
+    }
+
+
+
+
 }
