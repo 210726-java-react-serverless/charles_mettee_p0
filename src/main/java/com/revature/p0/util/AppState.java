06d4188b4092685f1941874adb4676dcd30622f6
metteec2 <chmettee@gmail.com>
2021-08-03 10:59:06 -0400

    Partial implementation of AppState class

--- src/main/java/com/revature/p0/util/AppState.java
+++ src/main/java/com/revature/p0/util/AppState.java
@@ -1,5 +1,42 @@
 package com.revature.p0.util;
 
+import com.revature.p0.screens.LoginScreen;
+import com.revature.p0.screens.WelcomeScreen;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
 public class AppState {
-    //#TODO implement AppState class;
+
+    private boolean appRunning;
+    private final ScreenRouter router;
+
+
+    public AppState() {
+        this.appRunning = true;
+        this.router = new ScreenRouter();
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+        router.addScreen(new WelcomeScreen(consoleReader, router));
+        router.addScreen(new LoginScreen(consoleReader, router));
+        router.addScreen(new RegisterScreen(consoleReader, router));
+        //#TODO add new screens to router as needed
+
+    }
+
+    public void startUp(){
+        router.navigate("/welcome");
+
+        while(appRunning){
+            try{
+                router.getCurrentScreen().render();
+            } catch (Exception e) {
+                e.printStackTrace(); //#TODO
+            }
+        }
+
+    }
+
+
 }
