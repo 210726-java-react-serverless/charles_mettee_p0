2a89b8284eac30efd43ffeb09d0e28ca4ad3a9bf
metteec2 <chmettee@gmail.com>
2021-08-03 09:04:58 -0400

    Following Wezley's Bookstore demo example; added fields, a constructor, and a method to the ScreenRouter class

--- src/main/java/com/revature/p0/util/ScreenRouter.java
+++ src/main/java/com/revature/p0/util/ScreenRouter.java
@@ -1,5 +1,27 @@
 package com.revature.p0.util;
 
+import com.revature.p0.screens.Screen;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ScreenRouter {
-    //#TODO implement ScreenRouter class;
+
+    private Screen currentScreen;
+    private Set<Screen> screenSet = new HashSet<>();
+
+    public ScreenRouter addScreen(Screen screen){
+        screenSet.add(screen);
+        return this;
+    }
+
+    public void navigate(String route){
+        for(Screen screen : screenSet){
+            if(screen.getRoute().equals(route)){
+                currentScreen = screen;
+                break;
+            }
+        }
+    }
+
+
 }
