83f633b451e28391884207b4a5b07c68a173ac0a
metteec2 <chmettee@gmail.com>
2021-08-06 17:27:06 -0400

    added UserSession class in util package

---
+++ src/main/java/com/revature/p0/util/UserSession.java
@@ -0,0 +1,24 @@
+package com.revature.p0.util;
+
+import com.revature.p0.models.User;
+public class UserSession {
+
+    private User currentUser;
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public boolean isActive() {
+        return currentUser != null;
+    }
+
+    public void closeSession() {
+        setCurrentUser(null);
+    }
+
+}
