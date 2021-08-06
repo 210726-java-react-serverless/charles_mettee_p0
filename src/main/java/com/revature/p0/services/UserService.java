c5215bdb29b6bf894176dd37a7557657571992d0
metteec2 <chmettee@gmail.com>
2021-08-06 18:13:36 -0400

    Added in unimplemented login and register methods from the example to the UserService class

--- src/main/java/com/revature/p0/services/UserService.java
+++ src/main/java/com/revature/p0/services/UserService.java
@@ -1,5 +1,72 @@
 package com.revature.p0.services;
 
+import com.revature.p0.models.User;
+import com.revature.p0.repositories.UserRepository;
+import com.revature.p0.util.UserSession;
+import com.revature.p0.util.exceptions.AuthenticationException;
+import com.revature.p0.util.exceptions.InvalidRequestException;
+import com.revature.p0.util.exceptions.ResourcePersistenceException;
+
 public class UserService {
+
+    private final UserRepository userRepo;
+    private final UserSession session;
+
+    public UserService(UserRepository userRepo, UserSession session) {
+        this.userRepo = userRepo;
+        this.session = session;
+    }
+
+    public UserSession getSession() {
+        return session;
+    }
+
+    public User register(User newUser) {
+
+        if (!isUserValid(newUser)) {
+            throw new InvalidRequestException("Invalid user data provided!");
+        }
+
+        if (userRepo.findUserByUsername(newUser.getUsername()) != null) {
+            throw new ResourcePersistenceException("Provided username is already taken!");
+        }
+
+        if (userRepo.findUserByEmail(newUser.getEmail()) != null) {
+            throw new ResourcePersistenceException("Provided username is already taken!");
+        }
+
+        return userRepo.save(newUser);
+
+    }
+
+    public User login(String username, String password) {
+
+        if (username == null || username.trim().equals("") || password == null || password.trim().equals("")) {
+            throw new InvalidRequestException("Invalid user credentials provided!");
+        }
+
+        User authUser = userRepo.findUserByCredentials(username, password);
+
+        if (authUser == null) {
+            throw new AuthenticationException("Invalid credentials provided!");
+        }
+
+        session.setCurrentUser(authUser);
+
+        return authUser;
+    }
+
+    public boolean isUserValid(User user) {
+        if (user == null) return false;
+        if (user.getFirstName() == null || user.getFirstName().trim().equals("")) return false;
+        if (user.getLastName() == null || user.getLastName().trim().equals("")) return false;
+        if (user.getEmail() == null || user.getEmail().trim().equals("")) return false;
+        if (user.getUsername() == null || user.getUsername().trim().equals("")) return false;
+        return user.getPassword() != null && !user.getPassword().trim().equals("");
+    }
+
+
+
+
 
 }
