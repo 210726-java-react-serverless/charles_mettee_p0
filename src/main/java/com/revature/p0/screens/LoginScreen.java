2143d40ff48a38ee11f6c5183db5f5d512509353
metteec2 <chmettee@gmail.com>
2021-08-07 15:22:41 -0400

    Updated LoginScreen constructor to accept UserService parameter, and added try-catch block

--- src/main/java/com/revature/p0/screens/LoginScreen.java
+++ src/main/java/com/revature/p0/screens/LoginScreen.java
@@ -1,26 +1,39 @@
 package com.revature.p0.screens;
 
+import com.revature.p0.models.User;
+import com.revature.p0.services.UserService;
 import com.revature.p0.util.ScreenRouter;
+import com.revature.p0.util.exceptions.AuthenticationException;
+
 import java.io.BufferedReader;
 
 public class LoginScreen extends Screen{
 
-    public LoginScreen(BufferedReader consoleReader, ScreenRouter router) {
+    private final UserService userService;
+
+    public LoginScreen(BufferedReader consoleReader, ScreenRouter router, UserService userService) {
         super("LoginScreen", "/Login", consoleReader, router);
+        this.userService = userService;
     }
 
     @Override
     public void render() throws Exception {
-        //#TODO implement rendering for LoginScreen
         System.out.println("\nWelcome to the login screen! Please enter your login information below\n");
 
-        System.out.print("Username\n> ");
+        System.out.print("\tUsername\n\t> ");
         String username = consoleReader.readLine();
 
-        System.out.print("Password\n> ");
+        System.out.print("\tPassword\n\t> ");
         String password = consoleReader.readLine();
 
-
-        System.exit(0);
+        try {
+            User authUser = userService.login(username, password);
+            System.out.println("Login successful!");
+            router.navigate("/dashboard");
+        } catch (AuthenticationException e) {
+            System.out.println("No user found with provided credentials!");
+            System.out.println("Navigating back to welcome screen...");
+            router.navigate("/welcome");
+        }
     }
 }
