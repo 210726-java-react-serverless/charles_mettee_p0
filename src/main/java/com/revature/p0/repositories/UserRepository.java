688f9dddb56c8802c9aeeb1ec312d0fd0e9a4d41
metteec2 <chmettee@gmail.com>
2021-08-07 15:17:35 -0400

    Included the findUserByCredentials method in UserRepository class and added necessary imports

--- src/main/java/com/revature/p0/repositories/UserRepository.java
+++ src/main/java/com/revature/p0/repositories/UserRepository.java
@@ -1,8 +1,43 @@
 package com.revature.p0.repositories;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.revature.p0.models.User;
+import com.revature.p0.util.MongoClientFactory;
+import com.revature.p0.util.exceptions.DataSourceException;
+import org.bson.Document;
 
 public class UserRepository implements CrudRepository<User> {
+
+    public User findUserByCredentials(String username, String password) {
+        try {
+            MongoClient mongoClient = MongoClientFactory.getInstance().getConnection();
+            MongoDatabase bookstoreDatabase = mongoClient.getDatabase("bookstore");
+            MongoCollection<Document> usersCollection = bookstoreDatabase.getCollection("users");
+            Document queryDoc = new Document("username", username).append("password", password);
+            Document authUserDoc = usersCollection.find(queryDoc).first();
+
+            if (authUserDoc == null) {
+                return null;
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            User authUser = mapper.readValue(authUserDoc.toJson(), User.class);
+            authUser.setId(authUserDoc.get("_id").toString());
+            return authUser;
+
+        } catch (JsonMappingException jme) {
+            jme.printStackTrace(); // TODO log this to a file
+            throw new DataSourceException("An exception occurred while mapping the document.", jme);
+        } catch (Exception e) {
+            e.printStackTrace(); // TODO log this to a file
+            throw new DataSourceException("An unexpected exception occurred.", e);
+        }
+    }
+
 
     @Override
     public User findById(int id) {
@@ -22,5 +57,15 @@ public class UserRepository implements CrudRepository<User> {
     @Override
     public boolean deleteById(int id) {
         return false;
+    }
+
+    // TODO implement this so that we can prevent multiple users from having the same username!
+    public User findUserByUsername(String username) {
+        return null;
+    }
+
+    // TODO implement this so that we can prevent multiple users from having the same email!
+    public User findUserByEmail(String email) {
+        return null;
     }
 }
