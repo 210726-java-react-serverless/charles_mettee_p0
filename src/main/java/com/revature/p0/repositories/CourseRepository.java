6c58939e6057cfd0b8740171207fec2a7e1d67d5
metteec2 <chmettee@gmail.com>
2021-08-07 21:40:20 -0400

    CourseRepository now implements CrudRepository, overridden methods empty

--- src/main/java/com/revature/p0/repositories/CourseRepository.java
+++ src/main/java/com/revature/p0/repositories/CourseRepository.java
@@ -1,4 +1,27 @@
 package com.revature.p0.repositories;
 
-public class CourseRepository {
+import com.revature.p0.models.Course;
+
+public class CourseRepository implements CrudRepository<Course> {
+
+
+    @Override
+    public Course findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Course save(Course newResource) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Course updatedResource) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return false;
+    }
 }
