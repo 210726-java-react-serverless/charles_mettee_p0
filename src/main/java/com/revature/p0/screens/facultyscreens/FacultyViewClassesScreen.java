832f3598b7f19fe6fcceee49a550610bf3f239f4
metteec2 <chmettee@gmail.com>
2021-08-07 21:25:43 -0400

    added FacultyViewClassesScreen

---
+++ src/main/java/com/revature/p0/screens/facultyscreens/FacultyViewClassesScreen.java
@@ -0,0 +1,16 @@
+package com.revature.p0.screens.facultyscreens;
+
+import com.revature.p0.screens.Screen;
+import com.revature.p0.util.ScreenRouter;
+
+import java.io.BufferedReader;
+
+public class FacultyViewClassesScreen extends Screen {
+    public FacultyViewClassesScreen(BufferedReader consoleReader, ScreenRouter router) {
+        super("FacultyViewClassesScreen", "/FacultyViewClasses", consoleReader, router);
+    }
+    @Override
+    public void render() throws Exception {
+
+    }
+}
